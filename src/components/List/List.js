143418d2a40da4274ba36942d697fda5912bb70a
Dawid Segieta <buler43221@gmail.com>
2023-03-16 21:12:49 +0100

    addColumnForm

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,18 +1,42 @@
 import styles from './List.module.scss';
 import Column from '../Column/Column';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import shortid from 'shortid';
+import ColumnForm from '../ColumnForm/ColumnForm';
+
 
 const List = () => {
+  const [columns, setColumns] = useState([
+      { id: 1, title: 'Books', icon: 'book' },
+      { id: 2, title: 'Movies', icon: 'film' },
+      { id: 3, title: 'Games', icon: 'gamepad' }
+  ]);
+
+ //useEffect(() => {
+
+        //setTimeout(() => {
+           // setColumns([...columns, { id: 4, title: 'Test column'}]);
+        //}, 2000);
+
+  //}, [columns]);
+
+  const addColumn = newColumn => {
+    setColumns([...columns, { id: shortid(), title: newColumn.title, icon: newColumn.icon }]);
+};
+    
     return (
     <div className={styles.list}>    
         <header className={styles.header}>
             <h2 className={styles.title}>Things to do <span>soon</span></h2>
         </header>
-        <p className={styles.description}>Interestings thing I want to check out</p>
+        <p className={styles.description}>
+            Interestings thing I want to check out
+        </p>
         <section className={styles.columns}>
-            <Column title= 'Books' icon="book"/> 
-            <Column title= 'Movies' icon="film" />
-            <Column title= 'Games' icon="gamepad" /> 
-        </section>   
+            {columns.map(column => <Column key={column.id} title={column.title} icon={column.icon} />)}
+        </section>
+        <ColumnForm action={addColumn} />
     </div>    
     );
   };
--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,6 +1,7 @@
 import styles from './SearchForm.module.scss'
 import TextInput from '../TextInput/TextInput';
 import Button from '../Button/Button';
+
 const SearchForm = (props) => {
     return (
         <form className={styles.searchForm}>
