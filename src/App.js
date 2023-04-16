0fa433a59ea2b4a589211eb2afe250f66e7bed15
Dawid Segieta <buler43221@gmail.com>
2023-04-16 19:35:02 +0200

    addHome-fixed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import Home from './components/Home/Home';
 import Container from './components/Container/Container'
 import NavBar from './components/Navbar/NavBar'
+import About from './components/About/About';
+import Favourite from './components/Favourite/Favourite';
+import { Routes, Route } from 'react-router-dom';
+import NotFound from "./components/NotFound/NotFound";
 
 const App = () => {
   return (
     <main>
       <NavBar />
       <Container>
-        <Home/>
+        <Routes>
+        <Route path='/' element={<Home />} />
+          <Route path='/favourite' element={<Favourite />} />
+          <Route path='/about' element={<About />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
       </Container>
-    </main>
+ </main>
   );
 }
 
--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -1,31 +1,51 @@
 import styles from "./NavBar.module.scss";
 import Container from "../Container/Container";
-import About from "../About/About";
-import Favourite from "../Favourite/Favourite";
+import { Link, NavLink } from "react-router-dom";
 
 const NavBar = () => {
-    return (
-        <nav className={styles.navBar}>
-        <Container>
-          <div className={styles.navWrapper}>
-            <a href='/'>
-              <span className='fa fa-tasks'></span>
-            </a>
-            <ul>
-              <li>
-                <a href='/'>Home</a>
-              </li>
-              <li>
-                <a href='/favorite'>Favorite</a>
-              </li>
-              <li>
-                <a href='/about'>About</a>
-              </li>
-            </ul>
-          </div>
-        </Container>
-      </nav>
-    );
+  return (
+    <nav className={styles.navBar}>
+      <Container>
+        <div className={styles.navWrapper}>
+          <a href="/">
+            <span className="fa fa-tasks"></span>
+          </a>
+          <ul>
+            <li>
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? styles.linkActive : undefined
+                }
+                to="/"
+              >
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? styles.linkActive : undefined
+                }
+                to="/favourite"
+              >
+                Favourite
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? styles.linkActive : undefined
+                }
+                to="/about"
+              >
+                About
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </Container>
+    </nav>
+  );
 };
 
 export default NavBar;
